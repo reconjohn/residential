f334d1d2f636c691b743465659797bcf7d97912e
Yohan <min25@uw.edu>
2018-12-07 15:36:25 -0800

    update

--- syntax/dv_table.R
+++ syntax/dv_table.R
@@ -1,8 +1,8 @@
-fhist <- regrs[, c(-1, -11)]
-par(mfrow=c(3,4))
-g_tab_ts <- for(i in 1:length(fhist)){
+fhist <- regrs[, c(-1, -8, -11, -12, -13)]
+par(mfrow=c(3,3))
+for(i in 1:length(fhist)){
   hist(fhist[[i]], main= paste("Histogram of\n", names(fhist)[i]),
        xlab= names(fhist)[i], col="gold")
   abline(v = median(fhist[[i]]), col="red", lwd=4)
-  text(median(fhist[[i]]), 0, round(median(fhist[[i]]),2), col = "blue")
+  text(median(fhist[[i]]), 7, round(median(fhist[[i]]),2), col = "blue")
 }
